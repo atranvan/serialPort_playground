ba9a90e0322c93757e10687b5cbb1f63ea1b7b63
atranvan <alexandra.tran.van.minh@gmail.com>
2020-02-04 18:22:17 +0000

    version with threading

--- serialPort_thread/serialPort_thread/Program.cs
+++ serialPort_thread/serialPort_thread/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
 
-namespace serialPort_thread
+public class PortChat
 {
-    class Program
+    static bool _continue;
+    static SerialPort _serialPort;
+
+    public static void Main()
     {
-        static void Main(string[] args)
+        string name;
+        string message;
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(Read);
+
+        // Create a new SerialPort object with default settings.
+        _serialPort = new SerialPort("COM25", 115200, Parity.None, 8, StopBits.One);
+
+        // Set the read/write timeouts
+        //_serialPort.ReadTimeout = 500;
+        //_serialPort.WriteTimeout = 500;
+
+        _serialPort.Open();
+        _continue = true;
+        readThread.Start();
+
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Type QUIT to exit");
+
+        while (_continue)
         {
+            message = Console.ReadLine();
+
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                //_serialPort.WriteLine(
+                //    String.Format("<{0}>: {1}", name, message));
+            }
+        }
+
+        readThread.Join();
+        _serialPort.Close();
+    }
+
+    public static void Read()
+    {
+        while (_continue)
+        {
+            try
+            {
+                string message = _serialPort.ReadLine();
+                Console.WriteLine(message);
+            }
+            catch (TimeoutException) { }
         }
     }
 }
