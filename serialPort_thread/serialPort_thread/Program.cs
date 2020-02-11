ae0fa2696b3bb12cdca0485396aa2ee3018021b9
atranvan <alexandra.tran.van.minh@gmail.com>
2020-02-11 16:25:33 +0000

    add blockingCollection queue

--- serialPort_thread/serialPort_thread/Program.cs
+++ serialPort_thread/serialPort_thread/Program.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.IO.Ports;
 using System.Threading;
+using System.Collections.Concurrent;
 
 public class PortChat
 {
     static bool _continue;
     static SerialPort _serialPort;
-
+    static BlockingCollection<string> outputQueue = new BlockingCollection<string>();
     public static void Main()
     {
-        string name;
-        string message;
-        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        //string name;
+        //string message;
+        //StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
         Thread readThread = new Thread(Read);
+
 
         // Create a new SerialPort object with default settings.
         _serialPort = new SerialPort("COM25", 115200, Parity.None, 8, StopBits.One);
-
+        Console.CancelKeyPress += delegate {
+            _continue = false;// call methods to clean up
+        };
         // Set the read/write timeouts
         //_serialPort.ReadTimeout = 500;
         //_serialPort.WriteTimeout = 500;
@@ -25,25 +29,30 @@ public class PortChat
         _continue = true;
         readThread.Start();
 
-        Console.Write("Name: ");
-        name = Console.ReadLine();
+        //Console.Write("Name: ");
+        //name = Console.ReadLine();
 
-        Console.WriteLine("Type QUIT to exit");
+        //Console.WriteLine("Type QUIT to exit");
 
         while (_continue)
         {
-            message = Console.ReadLine();
+            Console.WriteLine(outputQueue.Take());
 
-            if (stringComparer.Equals("quit", message))
-            {
-                _continue = false;
-            }
-            else
-            {
-                //_serialPort.WriteLine(
-                //    String.Format("<{0}>: {1}", name, message));
-            }
         }
+        //while (_continue)
+        //{
+        //    message = Console.ReadLine();
+
+        //    if (stringComparer.Equals("quit", message))
+        //    {
+        //        _continue = false;
+        //    }
+        //    else
+        //    {
+        //        //_serialPort.WriteLine(
+        //        //    String.Format("<{0}>: {1}", name, message));
+        //    }
+        //}
 
         readThread.Join();
         _serialPort.Close();
@@ -55,8 +64,9 @@ public class PortChat
         {
             try
             {
-                string message = _serialPort.ReadLine();
-                Console.WriteLine(message);
+                //string message = _serialPort.ReadLine();
+                outputQueue.Add(_serialPort.ReadLine());
+                //Console.WriteLine(message);
             }
             catch (TimeoutException) { }
         }
